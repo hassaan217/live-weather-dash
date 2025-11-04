b60863afc0a3fd5b5f49fbbf84f4f44cb08454d2
hassaan217 <shahidhassaan217@gmail.com>
2025-11-04 20:32:27 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://live-weather-dash-backend.vercel.app/api';
+const API_BASE_URL = 'https://weather-live-dashboard-backend.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
